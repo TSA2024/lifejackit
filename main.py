6576bb4091073d46a9d93665c913b4fd439d0964
1624252 <js226225@students.lcisd.org>
2023-11-28 21:11:11 -0600

    build: finish and separate hot reload file as rmain.py

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-from kaki.app import App
-from os import getcwd, path
-from kivy.factory import Factory
-
-# from kivymd.app import MDApp
-from kivymd.tools.hotreload.app import MDApp
+from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, FadeTransition
 
 from screens import *
 
 
-class AcaReady(App, MDApp):
-    KV_FILES = [
-        path.join(getcwd(), "acaready.kv")
-    ]
-    CLASSES = {
-        "MainScreen": "screens",
-    }
-    AUTORELOADER_PATHS = [
-        (".", {"recursive": True}),
-    ]
+class AcaReady(MDApp):
+    sm = None
 
-    def build_app(self, first=False):  # Previously build.
-        print("build_app called.")
+    def build(self):
         Window.size = (600, 850)
         self.sm = ScreenManager(transition=FadeTransition(duration=.5))
         self.sm.add_widget(MainScreen(name='main'))
-        return Factory.MainScreen()
+        return self.sm
 
 
 if __name__ == '__main__':
---
+++ rmain.py
@@ -0,0 +1,34 @@
+from os import getcwd, path
+from kivy.factory import Factory
+
+from kivymd.tools.hotreload.app import MDApp
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, FadeTransition
+
+from screens import *
+
+# Make sure you set DEBUG=1 in your PyCharm run configuration environment variables.
+
+
+class AcaReady(MDApp):
+    KV_FILES = [
+        path.join(getcwd(), "acaready.kv"),
+    ]
+    CLASSES = {
+        "MainScreen": "screens",
+    }
+    AUTORELOADER_PATHS = [
+        (".", {"recursive": True}),
+    ]
+    sm = None
+
+    def build_app(self, first=False):  # Previously build.
+        Window.size = (600, 850)
+        if self.sm is None:
+            self.sm = ScreenManager(transition=FadeTransition(duration=.5))
+            self.sm.add_widget(MainScreen(name='main'))
+        return Factory.MainScreen()
+
+
+if __name__ == '__main__':
+    AcaReady().run()
