15cd6e4b7275334c3355c383677a7100edfc7e9f
1624252 <js226225@students.lcisd.org>
2024-01-21 17:58:16 -0600

    refactor(login/signup): change back button for login/signup screen

--- rmain.py
+++ rmain.py
@@ -38,7 +38,7 @@ class LifeJackIt(MDApp):
             self.sm.add_widget(CreateAccountScreen(name='account'))
             self.sm.add_widget(LogInScreen(name='login'))
             sm = self.sm
-        return Factory.MainScreen()
+        return Factory.CreateAccountScreen()
 
 
 if __name__ == '__main__':
